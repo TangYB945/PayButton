0776e6550880a66a7dd8b75cac069e0b1114fcd5
Tonm <945931328@qq.com>
2020-05-04 16:49:19 +0800

    V1.0.2

--- Plugin.php
+++ Plugin.php
@@ -4,30 +4,29 @@
  * 添加打赏按钮 
  * @package PayButton
  * @author Tonm
- * @version 1.0.1
+ * @version 1.0.2
  * @link https://owo-bo.cn/
  * 
  */
 class PayButton_Plugin implements Typecho_Plugin_Interface
 {
-
     public static function activate()
     {
         Typecho_Plugin::factory('Widget_Archive')->footer = array('PayButton_Plugin', 'footer');
         Typecho_Plugin::factory('Widget_Archive')->header = array('PayButton_Plugin', 'header');
     	return '启用成功ヾ(≧▽≦*)o，请设置您的相关配置';
     }
-
+    
     public static function deactivate()
     {
     	return '你终究还是抛弃了我!';
     }
-
+    
     public static function config(Typecho_Widget_Helper_Form $form)
     {
+      	$up = json_decode(@file_get_contents('https://owo-bo.cn/effects/PayButton/owo.php'), true);
       	echo ('<style>.typecho-page-main a{background-color:#4F94CD;color:#FFFFFF;padding:2px 6px;border-radius:3px;line-height:15px;display:inline-block}.typecho-page-main a:hover{color:red}</style>');
-		$version = '1.0.1'; 
-		$up = json_decode(@file_get_contents('https://owo-bo.cn/effects/PayButton/owo.php'), true);
+		$version = '1.0.2'; 
       	$arr = json_decode(@file_get_contents(''.$up['url'].''.$up['bo'].''), true);
 		if(empty($arr['tag_name'])){
 			$new_version = '获取失败！';
@@ -41,7 +40,7 @@ class PayButton_Plugin implements Typecho_Plugin_Interface
         	$version_tips = '该插件有<font color="#ee9922">新版本</font> => <a href="'.$arr['browser_download_url'].'" target="_blank">点击下载</a>';
 			$new_version_out = '<font color="#ee9922">'.$new_version.'</font>';
 		}elseif($version > $new_version){
-            $version_tips = '版本比官方还高啦！请检查版本信息呀！';
+            $version_tips = '怎么怎么肥四，版本号比官方还高啦！请检联系管理员呀！';
           	$new_version_out = '<font color="#ee9922">'.$new_version.'</font>';
         }elseif($version = $new_version){
 			$version_tips = '您的插件为<font color="#ee9922">最新版本</font>，无需更新！';
@@ -56,23 +55,44 @@ class PayButton_Plugin implements Typecho_Plugin_Interface
         $form->addInput($jquery);
         $mdui = new Typecho_Widget_Helper_Form_Element_Radio('mdui',
             ['0' => _t('不加载'), '1' => _t('加载')],
-            '0', _t('是否加载外部mdui库'), _t('由于个人比较懒，就不想去密密麻麻的mdui里提取相应样式了，直接引用了官方文件<br>如果主题已加载就不需要加载了'));
+            '0', _t('是否加载外部mdui库'), _t('由于个人比较懒，就不想去密密麻麻的mdui里提取相应样式辽，直接引用了官方文件<br>如果主题已加载就不需要加载了'));
         $form->addInput($mdui);
-        $theme = new Typecho_Widget_Helper_Form_Element_Text('theme', null, '#3498db', _t('按钮颜色'), '这里填写十六进制颜色代码哦');
+        $felurl = new Typecho_Widget_Helper_Form_Element_Text('felurl', null, 'https://owo-bo.cn/pic/MainImg/PayButton/', _t('二维码外链地址'), '请填写二维码的外链地址呀！（eg：填到当前文件所在目录即可，链接末尾一定不要忘记反斜杠“/”呀）');
+        $form->addInput($felurl);
+        $alifelname = new Typecho_Widget_Helper_Form_Element_Text('alifelname', null, 'ali.png', _t('支付宝二维码名称'), '请填写支付宝二维码图片的名称呀！（eg：比如二维码名称是ali.png，直接填上即可）');
+        $form->addInput($alifelname);
+        $weifelname = new Typecho_Widget_Helper_Form_Element_Text('weifelname', null, 'wei.png', _t('微信二维码名称'), '请填写微信二维码图片的名称呀！（eg：比如二维码名称是wei.png，直接填上即可）');
+        $form->addInput($weifelname);
+        $tClor = new Typecho_Widget_Helper_Form_Element_Radio('tClor',
+            ['0' => _t('否'), '1' => _t('是')],
+            '0', _t('按钮是否启用自定义颜色'), _t('Bhao的主题，如果按钮需要自定义颜色，需开启选项，否则保持默认就行'));
+        $form->addInput($tClor);
+        $theme = new Typecho_Widget_Helper_Form_Element_Text('theme', null, '#3498db', _t('按钮颜色'), '如果按钮没有颜色，在这里填写十六进制颜色代码哦');
         $form->addInput($theme);
     }
-
+    
     public static function personalConfig(Typecho_Widget_Helper_Form $form){}
-
+    
     public static function render(){}
 
     public static function button(){
     	$theme = Typecho_Widget::widget('Widget_Options') -> Plugin('PayButton') -> theme;
+    	$felurl = Typecho_Widget::widget('Widget_Options') -> Plugin('PayButton') -> felurl;
+    	$alifelname = Typecho_Widget::widget('Widget_Options') -> Plugin('PayButton') -> alifelname;
+    	$weifelname = Typecho_Widget::widget('Widget_Options') -> Plugin('PayButton') -> weifelname;
+		$alisplionefelname = substr($alifelname,0,strrpos($alifelname ,"."));
+    	$alisplitowfelname = substr($alifelname,strripos($alifelname,".")+1);
+		$weisplionefelname = substr($weifelname,0,strrpos($weifelname ,"."));
+    	$weisplitowfelname = substr($weifelname,strripos($weifelname,".")+1);
+    	$tClor = Helper::options()->plugin('PayButton')->tClor;
+        if($tClor=="0") {
+        	$tClor='mdui-color-theme-accent';
+        }else {
+        	$tClor='';
+        }
     	echo '<!-- 感谢使用本插件 -->';
         echo '
-        <div class="article-btn">
-			<button style="background-color:'.$theme.'" onclick="datonmToggle();return false;" class="article-pay-btn mdui-btn mdui-btn-raised mdui-btn-dense mdui-color-theme-accent" title="打赏，支持一下"><i class="mdui-icon material-icons">monetization_on</i>打 赏</button>
-	
+			<button style="background-color:'.$theme.'" onclick="datonmToggle();return false;" class="article-pay-btn mdui-btn mdui-btn-raised mdui-btn-dense '.$tClor.'" title="打赏，支持一下"><i class="mdui-icon material-icons">monetization_on</i>打 赏</button>
 			<div class="hide_box"></div>
 			<div class="tonm_box">
 				<a class="tonm_close" href="#" onclick="datonmToggle();return false;" title="关闭"><img src="'.Helper::options()->pluginUrl.'/PayButton/reward/close.jpg" alt="取消" /></a>
@@ -80,15 +100,15 @@ class PayButton_Plugin implements Typecho_Plugin_Interface
 					<p>感谢您的支持，我会继续努力哒!</p>
 				</div>
 				<div class="tonm_payimg">
-					<img src="'.Helper::options()->pluginUrl.'/PayButton/reward/alipayimg.png" alt="扫码支持" title="扫一扫" />
+					<img src="'.$felurl.''.$alifelname.'" alt="扫码支持" title="扫一扫" />
 				</div>
 					<div class="pay_explain">扫码打赏<br>支付金额随意哦！</div>
 				<div class="tonm_payselect">
-					<div class="pay_item checked" data-id="alipay">
+					<div class="pay_item checked" data-id="'.$alisplionefelname.'">
 			    		<span class="radiobox"></span>
 			    		<span class="pay_logo"><img src="'.Helper::options()->pluginUrl.'/PayButton/reward/alipay.jpg" alt="支付宝" /></span>
 					</div>
-					<div class="pay_item" data-id="weipay">
+					<div class="pay_item" data-id="'.$weisplionefelname.'">
 			    		<span class="radiobox"></span>
 			    		<span class="pay_logo"><img src="'.Helper::options()->pluginUrl.'/PayButton/reward/wechat.jpg" alt="微信" /></span>
 					</div>
@@ -97,15 +117,19 @@ class PayButton_Plugin implements Typecho_Plugin_Interface
 					<p>打开<span id="tonm_pay_txt">支付宝</span>扫一扫，即可进行扫码打赏哦</p>
 				</div>
 			</div>
-		</div>';
+		';
         echo '
         <script type="text/javascript">
 			$(function(){
 				$(".pay_item").click(function(){
 					$(this).addClass("checked").siblings(".pay_item").removeClass("checked");
 					var dataid=$(this).attr("data-id");
-					$(".tonm_payimg img").attr("src","'.Helper::options()->pluginUrl.'/PayButton/reward/"+dataid+"img.png");
-					$("#tonm_pay_txt").text(dataid=="alipay"?"支付宝":"微信");
+					if ("'.$weisplitowfelname.'"=="'.$alisplitowfelname.'") {
+						$(".tonm_payimg img").attr("src","'.$felurl.'"+dataid+".png");
+					}else{
+						alert("请到后台检查两张图片扩展名是否一致！");
+					}
+					$("#tonm_pay_txt").text(dataid=="'.$alisplionefelname.'"?"支付宝":"微信");
 				});
 			});
 		</script>';
@@ -124,7 +148,7 @@ class PayButton_Plugin implements Typecho_Plugin_Interface
             echo '<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/mdui/0.4.3/css/mdui.min.css">';
         }
     }
-
+    
     public static function footer()
     {
         $jsUrl = Helper::options()->pluginUrl . '/PayButton/static/pay.js';
--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 ![GitHub](https://img.shields.io/github/license/TangYB945/PayButton?style=flat-square)
 ![GitHub release (latest by date)](https://img.shields.io/github/v/release/TangYB945/PayButton?style=flat-square&color=lightgodenrod)
+![GitHub code size in bytes](https://img.shields.io/github/languages/code-size/TangYB945/PayButton?style=flat-square)
 
 在文章中生成打赏按钮
 
@@ -10,7 +11,8 @@
 ### 下载
 获取插件的方法：请到[github](https://github.com/TangYB945/PayButton/)下载
 
-### 食用方法
+### 使用方法
+
 - 下载本插件，解压放到`usr/plugins/`目录中
 - 文件夹名改为`PayButton`
 - 登录管理后台，激活插件
@@ -26,7 +28,13 @@
 - 引用了静态MDUI样式文件库
 - 优化了部分手机适配问题
 
-#### 联系作者
+2020.04.24 v1.0.2
+- 修复按钮错位的问题
+- 优化关闭按钮样式
+- 兼容Bhao主题，按钮无法使用自定义颜色问题
+- 增加自定义二维码图片收款外链
+
+### 联系作者
 - Email:`blog@owo-bo.cn`
 - 欢迎访问 owo-bo.cn 一起学习讨论
 
